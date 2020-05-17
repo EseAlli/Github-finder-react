2578e5c36049f5a5d2e1aacea1ab6aeb8576bbf4
EseAlli <essealli.ea@gmail.com>
2020-05-17 22:32:00 +0100

    second commit

---
+++ src/components/users/User.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+    componentDidMount (){
+      this.props.getUser(this.props.match.params.login)
+      console.log(this.props)
+    }
+    render() {
+        const {name, avatar_url, location, bio, blog, login, htm_utl, followers, following, public_repos, public_gists, hirable} =this.props.user
+        const {loading} = this.props
+        return (
+            <div>
+                {name}
+            </div>
+        )
+    }
+}
+
+export default User
